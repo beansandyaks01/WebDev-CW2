3cf48c530a921c6a24336a9ea099923877359627
Binayak <gitbinayakmaharjan@gmail.com>
2023-04-25 11:43:10 +0545

    Initial Commit

---
+++ CarRental.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using CarRental.Domain.Shared;
+
+namespace CarRental.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }   
+        
+        public string Contact { get; set; }
+
+        public string Address { get; set; }
+    }
+}
